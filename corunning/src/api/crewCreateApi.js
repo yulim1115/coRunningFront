f0814c9e19ed83ecab5680815dbcc2f3d4692e16
Naseoi1022 <nsi1022@naver.com>
2025-12-04 21:48:11 +0900

    대시보드 제외 마이페이지 완성

--- corunning/src/api/crewCreateApi.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API_BASE = "/api/crew-board";
-
-export const crewCreateAPI = async (data) => {
-    return axios.post(API_BASE, data, {withCredentials: true});
-}
